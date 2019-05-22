21c077a636c436f38883b986a463a633330e9fa5
Gootjes <37846483+Gootjes@users.noreply.github.com>
2019-05-22 13:40:47 +0200

    v0.1.5: migrated to tidyselect::vars_select, added tests for attribute system

--- R/as_likert.R
+++ R/as_likert.R
@@ -1,4 +1,5 @@
 
+#' @importFrom tidyselect vars_select
 #' @importFrom rlang as_name
 #' @importFrom rlang quos
 #' @importFrom haven labelled
@@ -6,51 +7,51 @@
 as_likert <- function(.data, ..., .label = NULL, .labels = NULL, .complement = TRUE) {
   dots <- quos(...)
 
-  if(length(dots) > 0) {
-    for(i in seq_along(dots)) {
-      vname <- as_name(dots[[i]])
-      vattr <- attributes(.data[[vname]])
+  vnames <- vars_select(colnames(.data), !!!dots)
 
-      if(is_likerrt(vattr)) {
-        next
-      }
+  for(vname in vnames) {
+    vattr <- attributes(.data[[vname]])
 
-      if(!is_haven_labelled(vattr)) {
-        if(is.null(.labels)) {
-          stop(vname, " is not a haven labelled and no labels are specified")
-        }
-
-        if(.complement == TRUE) {
-          comp <- c()
-          for(v in unique(.data[[vname]])) {
-            if(v %in% .labels) {
-
-            } else {
-              comp[[as.character(v)]] <- v
-            }
-          }
-
-          .labels <- sort(c(.labels, comp))
-        }
-
-        labs <- sapply(X = seq_along(.labels), FUN = function(i) {
-          if(names(.labels)[i] == "") paste(.labels[i]) else names(.labels)[i]
-        })
-
-        names(.labels) <- labs
-
-        attr(.labels, which = "class") <- "likerrt_labels"
-
-        if(!is.null(.label))
-          attr(.label, which = "class") <- "likerrt_label"
-
-        .data[[vname]] <- labelled(.data[[vname]], labels = .labels, label = .label)
-      }
-
-      attributes(.data[[vname]])$class <- c("likerrt_likert", attributes(.data[[vname]])$class)
+    if(is_likerrt(vattr)) {
+      next
     }
 
+    if(!is_haven_labelled(vattr)) {
+      if(is.null(.labels)) {
+        stop(vname, " is not a haven labelled and no labels are specified")
+      }
+
+      if(.complement == TRUE) {
+        comp <- c()
+        for(v in unique(.data[[vname]])) {
+          if(v %in% .labels) {
+
+          } else {
+            comp[[as.character(v)]] <- v
+          }
+        }
+
+        .labels <- sort(c(.labels, comp))
+      }
+
+      labs <- sapply(X = seq_along(.labels), FUN = function(i) {
+        if(names(.labels)[i] == "") paste(.labels[i]) else names(.labels)[i]
+      })
+
+      names(.labels) <- labs
+
+      attr(.labels, which = "class") <- "likerrt_labels"
+
+      if(!is.null(.label))
+        attr(.label, which = "class") <- "likerrt_label"
+
+      .data[[vname]] <- labelled(.data[[vname]], labels = .labels, label = .label)
+    }
+
+    attributes(.data[[vname]])$class <- c("likerrt_likert", attributes(.data[[vname]])$class)
   }
+
+
 
   .data
 }
---
+++ tests/testthat/test_attributes.R
@@ -0,0 +1,18 @@
+context("test of attribute system")
+
+library(tidyverse)
+
+d <- data.frame(A = rep(1:7, 10), B = rep(7:1, 10)) %>%
+  as_likert(everything(), .label = "Variable", .labels = c("Disagree"=1, "Neutral"= 4, "Agree"=7))
+
+testthat::expect_equal(object = attributes(d$A)$label, expected = get_label(d)$A)
+testthat::expect_equal(object = attributes(attributes(d$A)$label)$class, expected = "likerrt_label")
+
+testthat::expect_equal(object = attributes(d$A)$labels, expected = get_labels(d)$A)
+testthat::expect_equal(object = attributes(attributes(d$A)$labels)$class, expected = "likerrt_labels")
+
+testthat::expect_equal(object = levels(as_factor(d$A)), expected = names(get_labels(d$A)))
+
+d %>% set_label(A, .value = "Variable A") %>% set_label(B, .value = "Variable B") -> d
+
+testthat::expect_equal(object = get_label(d$A), expected = structure("Variable A", class = "likerrt_label"))
