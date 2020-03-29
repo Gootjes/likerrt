e294617d06320115ed3bc845f5e2bd9476ce7100
Gootjes <37846483+Gootjes@users.noreply.github.com>
2020-03-29 17:51:10 +0200

    tweaked test_rescale

--- tests/testthat/test_rescale.R
+++ tests/testthat/test_rescale.R
@@ -1,137 +1,57 @@
-
-
-
 context("rescale")
 
 library(tidyverse)
 library(rlang)
+library(haven)
 
-d <- data.frame(A = c(1, 2, 3),
-                B = c(2, 3, 1),
-                C = c(3, 1, 2)) %>%
-  as_likert(A, B, C, .labels = c("A" = 1, "B" = 2, "C" = 3))
+n1 <- 100
+values1 <- 1:10
+gen <- function(values, n) sample(values, n, replace=TRUE, prob=rbeta(length(values), 1, 1))
+d1 <- data.frame(A = gen(values1, n1), B = gen(values1, n1), C = gen(values1, n1)) %>%
+  as_likert(A, B, C, .labels = c("lowest" = 1, 2, 3, 4, 5, 6, 7, 8, 9, "highest"=10))
 
-testthat::expect_equal(
-  object =
-    d %>%
-    likert_rescale(A, B, C, .min = 1, .max = 7),
-  expected = structure(
-    list(
-      A = structure(
-        c(1, 4, 7),
-        labels = structure(c(
-          A = 1,
-          B = 4, C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      ),
-      B = structure(
-        c(4, 7, 1),
-        labels = structure(c(
-          A = 1,
-          B = 4, C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      ),
-      C = structure(
-        c(7, 1, 4),
-        labels = structure(c(
-          A = 1,
-          B = 4, C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      )
-    ),
-    row.names = c(NA, -3L),
-    class = "data.frame"
-  )
-)
+i_na <- sort(sample(1:nrow(d1), size = nrow(d1)/4, replace = FALSE))
+
+d2 <- d1
+d2$A[i_na] <- NA
 
 
+a <- d1 %>% likert_rescale(A, B, C, .min = 0, .max = 1) %>% zap_labels()
+b <- d1 %>% mutate_all(~ (. - 1)/9) %>% zap_labels()
 
-d[2, "A"] <- NA
-
-testthat::expect_equal(
-  object =
-    d %>%
-    likert_rescale(A, B, C, .min = 1, .max = 7),
-  expected = structure(
-    list(
-      A = structure(
-        c(1, NA, 7),
-        labels = structure(c(
-          A = 1,
-          B = 4, C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      ),
-      B = structure(
-        c(4, 7, 1),
-        labels = structure(c(
-          A = 1, B = 4,
-          C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      ),
-      C = structure(
-        c(7, 1, 4),
-        labels = structure(c(
-          A = 1, B = 4,
-          C = 7
-        ), class = "likerrt_labels"),
-        class = c("likerrt_likert",
-                  "haven_labelled", "numeric")
-      )
-    ),
-    row.names = c(NA, -3L),
-    class = "data.frame"
-  )
-)
-
-a <- structure(list(A = structure(
-  c(1:7),
-  label = "Variable A",
-  labels = c(
-    `Do not agree` = 1,
-    `2` = 2,
-    `3` = 3,
-    `4` = 4,
-    `5` = 5,
-    `6` = 6,
-    `Does agree` = 7
-  ),
-  class = c("haven_labelled")
-)),
-row.names = c(NA, -7L),
-class = "data.frame") %>%
-  likert_rescale(A, .min = 1, .max = 3) %>% pluck("A") %>% round(2)
-
-b <-
-  structure(
-    c(1, 1.33, 1.67, 2, 2.33, 2.67, 3),
-    label = structure("Variable A (recoded)", class = "likerrt_label"),
-    labels = structure(
-      c(
-        `Do not agree` = 1,
-        `2` = 1.33333333333333,
-        `3` = 1.66666666666667,
-        `4` = 2,
-        `5` = 2.33333333333333,
-        `6` = 2.66666666666667,
-        `Does agree` = 3
-      ),
-      class = "likerrt_labels"
-    ),
-    class = c("likerrt_likert", "haven_labelled")
-  )
+testthat::expect_equal(a, b)
 
 
-testthat::expect_equal(object = get_label(a), expected = get_label(b))
-testthat::expect_equal(object = a, expected = b)
+a <- d1 %>% likert_rescale(A, B, C, .min = 1, .max = 0) %>% zap_labels()
+b <- d1 %>% mutate_all(~ 1-((. - 1)/9)) %>% zap_labels()
 
-testthat::expect_error(d %>% likert_rescale(A, B, C, .min = Inf, .max = NA))
+testthat::expect_equal(a, b)
+
+
+a <- d1 %>% likert_rescale(A, .min = 1, .max = -1, .suffix = "rec") %>% zap_labels()
+b <- d1 %>% mutate(Arec = 1+(((A - 1)/9)*-1)*2) %>% zap_labels()
+
+testthat::expect_equal(a, b)
+
+
+a <- d1 %>% likert_rescale(A, B, C, .min = 0, .max = 1)
+a_labels <- a %>% get_labels()
+
+testthat::expect_equal(a_labels$A %>% unclass() %>% as.vector(), seq(from = 0, to = 1, length.out = 10))
+
+testthat::expect_equal(a %>% as_factor(), d1 %>% as_factor())  # TODO: Is this desired?
+
+
+a <- d2 %>% likert_rescale(A, B, C, .min = 1, .max = -1) %>% zap_labels()
+b <- d2 %>% mutate_all(~ 1+(((. - 1)/9)*-1)*2) %>% zap_labels()
+
+testthat::expect_equal(a, b)
+
+
+a <- d2 %>% likert_rescale(A, .min = 1, .max = -1) %>% likert_scale(A, B, .name = "C", .assumptions = c('none'))
+b <- d2 %>% mutate(A = 1+(((A - 1)/9)*-1)*2) %>% likert_scale(A, B, .name = "C")
+
+testthat::expect_equal(a %>% zap_labels(), b %>% zap_labels())
+
+
+
